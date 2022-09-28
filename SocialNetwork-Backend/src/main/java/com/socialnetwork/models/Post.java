bd54a47f29a1a126c293f67095e57fadf8dfb56b
bhavanaburla <104483969+bhavanaburla@users.noreply.github.com>
2022-09-28 20:56:41 +0530

    Add files via upload

---
+++ SocialNetwork-Backend/src/main/java/com/socialnetwork/models/Post.java
@@ -0,0 +1,52 @@
+package com.socialnetwork.models;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Post {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String content;
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	private LocalDateTime createdon;
+	
+	public Post() {
+		this.createdon=LocalDateTime.now();
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public LocalDateTime getCreatedon() {
+		return createdon;
+	}
+	public void setCreatedon(LocalDateTime createdon) {
+		this.createdon = createdon;
+	}	
+}
---
+++ SocialNetwork-Backend/src/main/java/com/socialnetwork/services/PostService.java
@@ -0,0 +1,43 @@
+package com.socialnetwork.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.socialnetwork.models.Post;
+import com.socialnetwork.models.User;
+import com.socialnetwork.repos.PostRepository;
+
+@Service
+public class PostService {
+
+	@Autowired private PostRepository repo;
+	@Autowired private UserService service;
+	
+	public void save(Post post) {
+		repo.save(post);
+	}
+	
+	public void update(Post post) {
+		Post pp=findById(post.getId());
+		pp.setContent(post.getContent());
+		repo.save(pp);
+	}
+	
+	public Post findById(int id) {
+		return repo.findById(id).get();
+	}
+	
+	public List<Post> findByUserid(int id){
+		List<User> followingUsers=service.getAuthenticatedUser().getFollowingUsers();
+		List<Integer> followingIds=followingUsers.stream().map(x->x.getId()).collect(Collectors.toList());
+		followingIds.add(id);
+		return repo.findPostsByUserIdInOrderByIdDesc(followingIds);
+	}
+	
+	public void deletePost(int id) {
+		repo.deleteById(id);
+	}
+}
